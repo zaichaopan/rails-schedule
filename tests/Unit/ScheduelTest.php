21e23c8f389314c77d71b5a54bb4f05df7e992c0
zaichaopan <zaichaopan@gmail.com>
2018-05-20 09:40:06 -0700

    add more complex case for testing assign jobs

--- tests/Unit/ScheduelTest.php
+++ tests/Unit/ScheduelTest.php
@@ -40,77 +40,91 @@ class ScheduleTest extends TestCase
     }
 
     /** @test */
-    public function it_can_get_valid_user_job_preferences()
+    public function it_can_remove_empty_preferences()
     {
         $schedule = factory('App\Schedule')->create();
-        $foremanJobOne = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
-        $foremanJobTwo = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
-        $engineerJobOne = factory('App\Job')->states(Job::ENGINEER)->create(['schedule_id' => $schedule->id]);
-        $engineerJobTwo = factory('App\Job')->states(Job::ENGINEER)->create(['schedule_id' => $schedule->id]);
 
-        $kully = factory('App\User')->states(Job::FOREMAN)->create();
-        $slav = factory('App\User')->states(Job::ENGINEER)->create();
-
-        $kullyJobPreference = factory('App\UserJobPreference')->create([
-            'user_id' => $kully->id,
-            'schedule_id' => $schedule->id,
-            'preferences' => [$engineerJobOne->id, $foremanJobOne->id, $foremanJobTwo->id, 'abc']
-        ]);
-
-        $schedule = $schedule->fresh();
-        $validUserJobPreferences = $schedule->getValidUserJobPreferences();
-
-        $this->assertCount(1, $validUserJobPreferences);
-        $this->assertEquals($kullyJobPreference->id, $validUserJobPreferences->first()->id);
-        $this->assertEquals([$foremanJobOne->id, $foremanJobTwo->id], $validUserJobPreferences->first()->preferences);
+        $slav = factory('App\User')->create();
 
         $slavJobPreference = factory('App\UserJobPreference')->create([
             'user_id' => $slav->id,
             'schedule_id' => $schedule->id,
-            'preferences' => [$engineerJobTwo->id, $engineerJobOne->id, $foremanJobOne->id, $foremanJobTwo->id, 'abc', $foremanJobOne->id]
+            'preferences' => []
         ]);
 
-        $schedule = $schedule->fresh();
-        $validUserJobPreferences = $schedule->getValidUserJobPreferences();
-        $validSlavJobPreference = $validUserJobPreferences->first(function ($item) use ($slavJobPreference) {
-            return (int)$item->id === (int)$slavJobPreference->id;
-        });
+        $this->assertCount(0, $schedule->fresh()->getValidUserJobPreferences());
+    }
 
-        $this->assertCount(2, $validUserJobPreferences);
-        $this->assertEquals([$engineerJobTwo->id, $engineerJobOne->id, $foremanJobOne->id, $foremanJobTwo->id], $validSlavJobPreference->preferences);
+    /** @test */
+    public function it_can_remove_null_preferences()
+    {
+        $schedule = factory('App\Schedule')->create();
 
-        // job preferences that not for this schedule
-        $john = factory('App\User')->create();
-        $johnJobPreference = factory('App\UserJobPreference')->create([
-            'user_id' => $john->id,
-            'preferences' => [$engineerJobOne->id, $foremanJobOne->id, $foremanJobTwo->id]
-        ]);
+        $slav = factory('App\User')->create();
 
-        // job preferences that null or empty
-        $steve = factory('App\User')->create();
-        $steveJobPreference = factory('App\UserJobPreference')->create([
-            'user_id' => $steve->id,
+        $slavJobPreference = factory('App\UserJobPreference')->create([
+            'user_id' => $slav->id,
             'schedule_id' => $schedule->id,
             'preferences' => null
         ]);
 
-        $mike = factory('App\User')->create();
-        $mikeJobPreference = factory('App\UserJobPreference')->create([
-            'user_id' => $mike->id,
+        $this->assertCount(0, $schedule->fresh()->getValidUserJobPreferences());
+    }
+
+    /** @test */
+    public function it_can_remove_job_preferences_from_another_schedule()
+    {
+        $slav = factory('App\User')->states(Job::FOREMAN)->create();
+        $schedule = factory('App\Schedule')->create();
+        $jobInSchedule = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
+        $jobFromDifferentSchedule = factory('App\Job')->create();
+
+        $slavJobPreference = factory('App\UserJobPreference')->create([
+            'user_id' => $slav->id,
             'schedule_id' => $schedule->id,
-            'preferences' => []
+            'preferences' => [$jobInSchedule->id, $jobFromDifferentSchedule->Id]
         ]);
 
-        $schedule = $schedule->fresh();
-        $validUserJobPreferences = $schedule->getValidUserJobPreferences();
-        $validUserJobPreferenceIds = $validUserJobPreferences->pluck('id')->all();
+        $validUserJobPreferences = $schedule->fresh()->getValidUserJobPreferences();
+        $this->assertCount(1, $validUserJobPreferences);
+        $this->assertEquals([$jobInSchedule->id], $validUserJobPreferences->first()->preferences);
+    }
 
-        $this->assertCount(2, $validUserJobPreferences);
-        $this->assertTrue(in_array($kullyJobPreference->id, $validUserJobPreferenceIds));
-        $this->assertTrue(in_array($slavJobPreference->id, $validUserJobPreferenceIds));
-        $this->assertFalse(in_array($johnJobPreference->id, $validUserJobPreferenceIds));
-        $this->assertFalse(in_array($steveJobPreference->id, $validUserJobPreferenceIds));
-        $this->assertFalse(in_array($mikeJobPreference->id, $validUserJobPreferenceIds));
+    /** @test */
+    public function it_can_remove_unqualified_job_preferences()
+    {
+        $slav = factory('App\User')->states(Job::FOREMAN)->create();
+        $schedule = factory('App\Schedule')->create();
+        $foremanJob = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
+        $engineerJob = factory('App\Job')->states(Job::ENGINEER)->create(['schedule_id' => $schedule->id]);
+
+        $slavJobPreference = factory('App\UserJobPreference')->create([
+            'user_id' => $slav->id,
+            'schedule_id' => $schedule->id,
+            'preferences' => [$foremanJob->id, $engineerJob->Id]
+        ]);
+
+        $validUserJobPreferences = $schedule->fresh()->getValidUserJobPreferences();
+        $this->assertCount(1, $validUserJobPreferences);
+        $this->assertEquals([$foremanJob->id], $validUserJobPreferences->first()->preferences);
+    }
+
+    /** @test */
+    public function it_can_remove_duplicate_job_preferences()
+    {
+        $slav = factory('App\User')->states(Job::FOREMAN)->create();
+        $schedule = factory('App\Schedule')->create();
+        $foremanJob = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
+
+        $slavJobPreference = factory('App\UserJobPreference')->create([
+            'user_id' => $slav->id,
+            'schedule_id' => $schedule->id,
+            'preferences' => [$foremanJob->id, $foremanJob->id]
+        ]);
+
+        $validUserJobPreferences = $schedule->fresh()->getValidUserJobPreferences();
+        $this->assertCount(1, $validUserJobPreferences);
+        $this->assertEquals([$foremanJob->id], $validUserJobPreferences->first()->preferences);
     }
 
     /** @test */
@@ -118,40 +132,84 @@ class ScheduleTest extends TestCase
     {
         $schedule = factory('App\Schedule')->create();
 
-        $foremanJobOne  = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
+        $foremanJobOne = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
         $foremanJobTwo = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
+        $foremanJobThree = factory('App\Job')->states(Job::FOREMAN)->create(['schedule_id' => $schedule->id]);
         $engineerJobOne = factory('App\Job')->states(Job::ENGINEER)->create(['schedule_id' => $schedule->id]);
+        $engineerJobTwo = factory('App\Job')->states(Job::ENGINEER)->create(['schedule_id' => $schedule->id]);
 
-        $slav = factory('App\User')->states(Job::ENGINEER)->create(['engineer_since' => today(), 'foreman_since' => today()]);
+        $slav = factory('App\User')->states(Job::ENGINEER)->create(['engineer_since' => today()]);
         $kully = factory('App\User')->states(Job::ENGINEER)->create(['engineer_since' => today()->subDays(2)]);
         $mike = factory('App\User')->states(Job::FOREMAN)->create(['foreman_since' => today()->subDays(2)]);
+        $alex = factory('App\User')->states(Job::FOREMAN)->create(['foreman_since' => today()->subDays(3)]);
+        $steve = factory('App\User')->states(Job::FOREMAN)->create(['foreman_since' => today()->subDays(5)]);
 
         $slavJobPreference = factory('App\UserJobPreference')->create([
             'schedule_id' => $schedule->id,
             'user_id' => $slav->id,
-            'preferences' => [$engineerJobOne->id, $foremanJobTwo->id, $foremanJobOne->id]
+            'preferences' => [
+                $engineerJobOne->id,
+                $engineerJobTwo->id,
+                $foremanJobTwo->id,
+                $foremanJobOne->id,
+                $foremanJobThree->id
+            ]
         ]);
 
         $kullyJobPreference = factory('App\UserJobPreference')->create([
             'schedule_id' => $schedule->id,
             'user_id' => $kully->id,
-            'preferences' => [$engineerJobOne->id, $foremanJobOne->id, $foremanJobTwo->id]
+            'preferences' => [
+                $engineerJobOne->id,
+                $engineerJobTwo->id,
+                $foremanJobOne->id,
+                $foremanJobTwo->id,
+                $foremanJobThree->id
+            ]
         ]);
 
-        $MikeJobPreference = factory('App\UserJobPreference')->create([
+        $mikeJobPreference = factory('App\UserJobPreference')->create([
             'schedule_id' => $schedule->id,
             'user_id' => $mike->id,
-            'preferences' => [$foremanJobTwo->id, $foremanJobOne->id]
+            'preferences' => [
+                $foremanJobTwo->id,
+                $foremanJobThree->id,
+                $foremanJobOne->id
+            ]
+        ]);
+
+        $alexJobPreference = factory('App\UserJobPreference')->create([
+            'schedule_id' => $schedule->id,
+            'user_id' => $alex->id,
+            'preferences' => [
+                $foremanJobTwo->id,
+                $foremanJobOne->id,
+                $foremanJobThree->id
+            ]
+        ]);
+
+        $steveJobPreference = factory('App\UserJobPreference')->create([
+            'schedule_id' => $schedule->id,
+            'user_id' => $steve->id,
+            'preferences' => [
+                $foremanJobThree->id,
+                $foremanJobOne->id,
+                $foremanJobTwo->id
+            ]
         ]);
 
         $schedule = $schedule->fresh();
         $schedule->assignJobs();
-        $engineerJobOne = $engineerJobOne->fresh();
-        $foremanJobTwo = $foremanJobTwo->fresh();
         $foremanJobOne = $foremanJobOne->fresh();
+        $foremanJobTwo = $foremanJobTwo->fresh();
+        $foremanJobThree = $foremanJobThree->fresh();
+        $engineerJobOne = $engineerJobOne->fresh();
+        $engineerJobTwo = $engineerJobTwo->fresh();
 
-        $this->assertEquals($slav->id, $foremanJobOne->user_id);
-        $this->assertEquals($mike->id, $foremanJobTwo->user_id);
+        $this->assertEquals($mike->id, $foremanJobOne->user_id);
+        $this->assertEquals($alex->id, $foremanJobTwo->user_id);
+        $this->assertEquals($steve->id, $foremanJobThree->user_id);
         $this->assertEquals($kully->id, $engineerJobOne->user_id);
+        $this->assertEquals($slav->id, $engineerJobTwo->user_id);
     }
 }
